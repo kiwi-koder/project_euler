99cebacdd90a0a2c58552788eda29477650ed061
Joshua Rippon <joshua@pomerolpartners.com>
2018-06-15 15:42:59 +0100

    quadratic_primes_27.js solved

---
+++ js/quadratic_primes_27.js
@@ -0,0 +1,28 @@
+const isPrime = num => {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if (num % i === 0) return false;
+    return num > 1;
+};
+
+const quadratic = (a, b, n) => n ** 2 + a * n + b;
+
+let maxA = 0;
+let maxB = 0;
+let maxN = 0;
+for (let a = -9999; a < 9999; a++) {
+    for (let b = -9999; b < 9999; b++) {
+        let n = 0;
+        while (isPrime(quadratic(a, b, n))) {
+            n++;
+        }
+        if (n > maxN) {
+            maxA = a;
+            maxB = b;
+            maxN = n;
+        }
+    }
+}
+
+console.log(isPrime(-4));
+
+console.log({ maxA, maxB, maxN, AtimesB: maxA * maxB });
